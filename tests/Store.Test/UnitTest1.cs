39e7f33cc429db398356d6024b7ddf577e2ae91c
Lucas Almeida <lucasags.dev@gmail.com>
2024-10-13 20:47:44 -0300

    Teste rota Cliente
    rota Obter Por Data

--- tests/Store.Test/UnitTest1.cs
+++ tests/Store.Test/UnitTest1.cs
@@ -1,11 +1,21 @@
+using Core.Entity;
+
 namespace Store.Test
 {
-	public class UnitTest1
+	public class ClienteRepositoryTeste
 	{
 		[Fact]
-		public void Test1()
+		public void ObterPorDataTeste()
 		{
+			Cliente cliente = new Cliente
+			{
+				Id = 5, 
+				Nome = "Lucas", 
+				Cpf = "03416427114"
+			};
 
+			string dataIni = "2024-04-01";
+			string dataFin = "2024-10-01";
 		}
 	}
 }
--- tests/Store.Test/obj/Debug/net8.0/Store.Test.AssemblyInfo.cs
+++ tests/Store.Test/obj/Debug/net8.0/Store.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa444c9ad2f46468e5152ba16e13349520166ec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874f6a94abbd8540b39250d79e71904367f70037")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
